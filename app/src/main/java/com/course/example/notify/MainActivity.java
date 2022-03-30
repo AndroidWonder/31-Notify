12fe7d2fa0c09d54cf538ffc81555f51f59bc3a2
Android Wonder <jim.pepe@verizon.net>
2022-03-30 19:57:49 -0400

    Create a channel

--- app/src/main/java/com/course/example/notify/MainActivity.java
+++ app/src/main/java/com/course/example/notify/MainActivity.java
@@ -1,82 +1,83 @@
 package com.course.example.notify;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private NotificationManager NotificationManager;
-    private NotificationCompat.Builder builder = null;
-    private int SIMPLE_NOTFICATION_ID = 25;
-    private String CHANNEL_ID = "11";
+
+    private NotificationManager mManager;
+    public static final String ANDROID_CHANNEL_ID = "com.chikeandroid.tutsplustalerts.ANDROID";
+    public static final String ANDROID_CHANNEL_NAME = "ANDROID CHANNEL";
+    public static final int SIMPLE_NOTFICATION_ID = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationManager =
-                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        String title = "Nice Try";
+        String body = "foobar";
 
-        createNotificationChannel();
-
-        // Create an explicit intent for an Activity
-        Intent intent = new Intent(this, Widgets.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-        //the pending intent will outlive this app
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 9, intent,0 );
-
-        builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.droid)
-                .setContentTitle("Android Notification")
-                .setContentText("Tap to open new activity")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
+        //set up buttons for notifications and cancel
         Button start = (Button) findViewById(R.id.btn_showsample);
         Button cancel = (Button) findViewById(R.id.btn_clear);
 
-        //notify
+        // create android channel
+        NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID,
+                ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+        // Sets whether notifications posted to this channel should display notification lights
+        androidChannel.enableLights(true);
+        // Sets whether notification posted to this channel should vibrate.
+        androidChannel.enableVibration(true);
+        // Sets the notification light color for notifications posted to this channel
+        androidChannel.setLightColor(Color.GREEN);
+        // Sets whether notifications posted to this channel appear on the lockscreen or not
+        androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mManager.createNotificationChannel(androidChannel);
+
+        //create Intent and PendingIntent
+        Intent intent = new Intent(this, Widgets.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,0);
+
+        //create notification
+        final Notification.Builder nb = new Notification.Builder(getApplicationContext(), ANDROID_CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.droid)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        //mManager.notify(SIMPLE_NOTFICATION_ID, nb.build());
+
         start.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
                 //notify() in response to button click.
-                NotificationManager.notify(SIMPLE_NOTFICATION_ID, builder.build());
+                mManager.notify(SIMPLE_NOTFICATION_ID, nb.build());
+
             }
         });
 
-        //clear notification
         cancel.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
-                NotificationManager.cancel(SIMPLE_NOTFICATION_ID);
+
+                mManager.cancel(SIMPLE_NOTFICATION_ID);
             }
         });
 
     }
 
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
 }
