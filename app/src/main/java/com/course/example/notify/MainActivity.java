e0b35346afcffc183018e872fe569d7c0982c8f2
Android Wonder <jim.pepe@verizon.net>
2022-03-30 23:16:38 -0400

    set vibration pattern

--- app/src/main/java/com/course/example/notify/MainActivity.java
+++ app/src/main/java/com/course/example/notify/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
         // Sets whether notifications posted to this channel should display notification lights
         androidChannel.enableLights(true);
+        androidChannel.setVibrationPattern(new long[] { 100, 100 });
         // Sets whether notification posted to this channel should vibrate.
         //Needs permission in Manifest
         androidChannel.enableVibration(true);
